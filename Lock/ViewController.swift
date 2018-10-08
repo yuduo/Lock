077681e5490861badfdb5b0cf3824c8d1ebfb339
yong hu <hu_yong_chang@outlook.com>
2018-10-08 11:04:36 +0800

    Initial Commit

---
+++ Lock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lock
+//
+//  Created by yong hu on 08/10/2018.
+//  Copyright © 2018 yong hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
