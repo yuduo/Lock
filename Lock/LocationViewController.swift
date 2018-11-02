4d32b0269d198bf97fa11a5b991fc044e6202f22
yong hu <hu_yong_chang@outlook.com>
2018-11-02 10:43:50 +0800

    add contain

---
+++ Lock/LocationViewController.swift
@@ -0,0 +1,113 @@
+//
+//  MapViewController.swift
+//  Lock
+//
+//  Created by yong hu on 12/10/2018.
+//  Copyright © 2018 yong hu. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController ,BMKMapViewDelegate,CLLocationManagerDelegate{
+    var _mapView: BMKMapView?
+    let locationManager = CLLocationManager()
+    var LocationArray:[Location] = []
+    var location:CLLocationCoordinate2D=CLLocationCoordinate2DMake(Double(120.665441), Double(31.2043183));
+    var name:String=""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        _mapView = BMKMapView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        self.view.addSubview(_mapView!)
+        _mapView?.delegate=self
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+        
+        //显示定位图层
+        _mapView?.showsUserLocation = true
+        //设置定位的状态为普通定位模式
+        _mapView?.userTrackingMode = BMKUserTrackingModeNone
+        
+        addAnnotation(location: location, name: name)
+        _mapView?.setCenter(location, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        _mapView?.viewWillAppear()
+        _mapView?.delegate = self  // 此处记得不用的时候需要置nil，否则影响内存的释放
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        _mapView?.viewWillDisappear()
+        _mapView?.delegate = nil // 不用时，置nil
+    }
+    
+    // 根据anntation生成对应的View
+    func mapView(mapView: BMKMapView!, viewForAnnotation annotation: BMKAnnotation!) -> BMKAnnotationView! {
+        
+        //        //annotation
+        let annotationViewID = "renameMark"
+        var annotationView:BMKPinAnnotationView? = _mapView?.dequeueReusableAnnotationView(withIdentifier: annotationViewID) as? BMKPinAnnotationView
+        
+        if(annotationView == nil){
+            
+            annotationView = BMKPinAnnotationView.init(annotation:annotation, reuseIdentifier:annotationViewID)
+        }
+        
+        //设置颜色
+        //            annotationView?.pinColor = BMKPinAnnotationColorPurple
+        //从天上掉下来效果
+        annotationView!.animatesDrop = true
+        //设置不可拖拽
+        annotationView!.isDraggable = false
+        annotationView!.image = UIImage(named:"sina")
+        return annotationView
+    }
+    
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+    }
+    
+    func addAnnotation(location:CLLocationCoordinate2D,name:String){
+
+        let annotation:BMKPointAnnotation=BMKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2DMake(Double(location.latitude), Double(location.longitude));
+        annotation.title = name;
+        _mapView?.addAnnotation(annotation)
+
+        
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,16 @@
+platform :ios, '8.0' #手机的系统
+use_frameworks!
+target 'Lock' do #工程名字
+  pod 'BaiduMapKit' #百度地图SDK
+  pod 'SwiftSocket'
+  pod 'Socket.IO-Client-Swift', '~> 13.3.0'
+  pod 'SQLite.swift', '~> 0.11.5'
+  pod 'CryptoSwift'
+  pod 'SwiftMessages'
+  pod 'DropDown', '~> 2.3.2'
+  pod 'TDBadgedCell'
+  pod 'DLRadioButton'
+  pod 'KMPlaceholderTextView'
+  use_frameworks!  
+  pod 'BluetoothKit'
+end
